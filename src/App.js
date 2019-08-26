d8786b3f4039b2c1d26ea497343caf772f562835
todorpavlovic <todorpavlovic@gmail.com>
2019-08-26 15:53:10 +0200

    components added, styles added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <HomePage />
     </div>
-  );
+  );  
+}
 }
 
 export default App;
