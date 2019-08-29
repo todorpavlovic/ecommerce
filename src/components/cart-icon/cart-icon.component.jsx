cf4b5babfad37239204c50a538fc8d3301cf011b
todorpavlovic <todorpavlovic@gmail.com>
2019-08-29 17:09:41 +0200

    added reselectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => {
     return (
@@ -18,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    itemCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
