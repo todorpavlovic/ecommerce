8813b8262d6bfefb657e5722cae73c9d70e1df88
todorpavlovic <todorpavlovic@gmail.com>
2019-08-29 16:42:33 +0200

    added counter to shop icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,11 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 
-const CartIcon = ({toggleCartHidden}) => {
-    console.log(toggleCartHidden)
+const CartIcon = ({toggleCartHidden, itemCount}) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{ itemCount }</span>
         </div>
     )
 }
@@ -19,5 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
+})
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
